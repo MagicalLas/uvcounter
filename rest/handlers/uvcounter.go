925c638c8fb8e64308877ad6fa73ef5da1dcdebb
Las <haho013039@gmail.com>
2025-05-02 19:55:00 +0900

    sync pool이 그렇게 좋지 않아서 롤백ㅋ

--- rest/handlers/uvcounter.go
+++ rest/handlers/uvcounter.go
@@ -1,20 +1,12 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v3"
+
 	"gomod.usaken.org/uvcounter/application"
-	"strconv"
-	"sync"
 )
-
-var bufPool = sync.Pool{
-	New: func() interface{} {
-		b := make([]byte, 0, 64)
-		return &b
-	},
-}
-
-var countPrefix = []byte("count: ")
 
 func GetUVCounter(c fiber.Ctx) error {
 	counterID := c.Get("counterID", "0")
@@ -22,15 +14,5 @@ func GetUVCounter(c fiber.Ctx) error {
 	service := application.UVCounterService{}
 	count := service.GetUVCounter(counterID)
 
-	bufPtr := bufPool.Get().(*[]byte)
-	buf := (*bufPtr)[:0]
-
-	buf = append(buf, countPrefix...)
-	buf = strconv.AppendInt(buf, count, 10)
-
-	err := c.Send(buf)
-
-	bufPool.Put(bufPtr)
-
-	return err
+	return c.Send([]byte(fmt.Sprintf("count: %d", count)))
 }
