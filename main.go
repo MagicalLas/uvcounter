3029e35e42b5b346896726e6e277b6e6550ef41c
Las <haho013039@gmail.com>
2025-05-02 21:50:46 +0900

    add mem limit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"gomod.usaken.org/uvcounter/monitor"
 	"gomod.usaken.org/uvcounter/rest"
@@ -9,6 +10,8 @@ import (
 )
 
 func main() {
+	debug.SetMemoryLimit(16 << 30)
+
 	fmt.Println("uv counter system started.")
 
 	monitor.RunPprofServer()
