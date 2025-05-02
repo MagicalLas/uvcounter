db303f15f67e0510a7afb8d74a652f8d2b44a87e
Las <haho013039@gmail.com>
2025-05-02 19:35:20 +0900

    add sync pool buffer

--- rest/handlers/uvcounter.go
+++ rest/handlers/uvcounter.go
@@ -1,10 +1,20 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v3"
 	"gomod.usaken.org/uvcounter/application"
+	"strconv"
+	"sync"
 )
+
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		b := make([]byte, 0, 64)
+		return &b
+	},
+}
+
+var countPrefix = []byte("count: ")
 
 func GetUVCounter(c fiber.Ctx) error {
 	counterID := c.Get("counterID", "0")
@@ -12,5 +22,15 @@ func GetUVCounter(c fiber.Ctx) error {
 	service := application.UVCounterService{}
 	count := service.GetUVCounter(counterID)
 
-	return c.Send([]byte(fmt.Sprintf("count: %d", count)))
+	bufPtr := bufPool.Get().(*[]byte)
+	buf := (*bufPtr)[:0]
+
+	buf = append(buf, countPrefix...)
+	buf = strconv.AppendInt(buf, count, 10)
+
+	err := c.Send(buf)
+
+	bufPool.Put(bufPtr)
+
+	return err
 }
