3fe7fd323ecd0297f401bd6abeacf2d6f85db7fd
Las <haho013039@gmail.com>
2025-05-02 18:05:14 +0900

    disable tcp options

--- rest/listener_linux.go
+++ rest/listener_linux.go
@@ -3,7 +3,6 @@
 package rest
 
 import (
-	"golang.org/x/sys/unix"
 	"log"
 	"net"
 	"syscall"
@@ -31,15 +30,15 @@ func Listener(addr string) net.Listener {
 func setSocketOptionsForPerformance(c syscall.RawConn) (err error) {
 	// tcp nodalay와 tcp quickack을 활성화합니다.
 	c.Control(func(fd uintptr) {
-		err = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, TCP_QUICKACK, 1)
-		if err != nil {
-			log.Printf("Failed to set TCP_QUICKACK: %v", err)
-		}
+		//err = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, TCP_QUICKACK, 1)
+		//if err != nil {
+		//	log.Printf("Failed to set TCP_QUICKACK: %v", err)
+		//}
 
-		err = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, unix.TCP_NODELAY, 1)
-		if err != nil {
-			log.Printf("Failed to set TCP_NODELAY: %v", err)
-		}
+		//err = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, unix.TCP_NODELAY, 1)
+		//if err != nil {
+		//	log.Printf("Failed to set TCP_NODELAY: %v", err)
+		//}
 	})
 	return
 }
