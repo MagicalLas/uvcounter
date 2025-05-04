37b833f0acefc420a8edfe757db01427808bc6e1
Las <haho013039@gmail.com>
2025-05-04 17:17:15 +0900

    remove static mem limit

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"runtime/debug"
-
 	"gomod.usaken.org/uvcounter/monitor"
 	"gomod.usaken.org/uvcounter/rest"
 	"gomod.usaken.org/uvcounter/spine"
 )
 
 func main() {
-	debug.SetMemoryLimit(16 << 30)
-
 	fmt.Println("uv counter system started.")
 
 	monitor.RunPprofServer()
