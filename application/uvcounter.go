47703b634d3996b58ecef9f708a4e77969e2b950
Las <haho013039@gmail.com>
2025-05-04 17:27:15 +0900

    add redis get

--- application/uvcounter.go
+++ application/uvcounter.go
@@ -1,11 +1,22 @@
 package application
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"fmt"
+	"time"
+	
+	"github.com/redis/go-redis/v9"
+)
 
 type UVCounterService struct {
 	RedisClient *redis.Client
 }
 
-func (s UVCounterService) GetUVCounter(id string) int64 {
+func (s *UVCounterService) GetUVCounter(id string) int64 {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	s.RedisClient.Get(ctx, fmt.Sprintf("sampleKey:%s", id))
+
 	return 0
 }
